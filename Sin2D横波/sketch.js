71281d91bca3ef574ba6de39a40e974b440124c5
牛玉龙 <niuyulong@localhost.com>
2018-01-09 18:52:47 +0800

    修改Sin2D横波。优化效果。

--- Sin2D横波/sketch.js
+++ Sin2D横波/sketch.js
@@ -2,7 +2,7 @@
 
 var waveSystem;
 function setup() {
-  createCanvas(400,400)
+  createCanvas(1000,800)
   background(137,190,223);
   frameRate(15);
 
@@ -15,15 +15,27 @@ function WaveSystem() {
   this.waves = [];
 
   this.setup = function () {
-    for (var i = 0; i < 5;i++) {
-      // if (i==0) {
-      //   var origin = createVector(-width,random(0,height));
-      //   this.waves[i] = new Wave(origin,10,100,2);
-      // } 
-      // else {
-        var origin = createVector(random(- width + 10,width*2),random(-height,2*height));
+    for (var i = 0; i < 4;i++) {
+      if (i==0) {
+      var origin = createVector(-1000,-1000);//createVector(random(- width *10,- width *5),random(-height,2*height));
+        this.waves[i] = new Wave(origin,random(-10,10),random(width/10,width),random(2,10));
+      } 
+      else if(i ==1) {
+        var origin = createVector(-1000,height/2);//createVector(random(- width *10,- width *5),random(-height,2*height));
+        this.waves[i] = new Wave(origin,random(-10,10),random(width/10,width),random(2,10));
+      }
+      else if(i ==2) {
+        var origin = createVector(-1000,height/4);//createVector(random(- width *10,- width *5),random(-height,2*height));
+        this.waves[i] = new Wave(origin,random(-10,10),random(width/10,width),random(2,10));
+      }
+      else if(i ==3) {
+        var origin = createVector(-1000,1000);//createVector(random(- width *10,- width *5),random(-height,2*height));
         this.waves[i] = new Wave(origin,random(-10,10),random(width/10,width/2),random(2,10));
-      // }
+      } else {
+        var origin = createVector(random(- width *10,- width *5),random(-height,2*height));
+        this.waves[i] = new Wave(origin,random(-2,2),random(width/10,width*10),random(2,10));
+
+      }
     }
     print(this.waves.length);
   }
@@ -35,9 +47,9 @@ function WaveSystem() {
     }
 
     var scl = 0.04
-    var noiseValue = noise(location.x + frameCount * scl,location.y + frameCount * scl); 
+    var noiseValue = noise(location.x*0.01,location.y*0.01, frameCount * scl); 
 
-    z += map(noiseValue,0,1,-20,20);
+    z += map(noiseValue,0,1,-5,5);
     return z;
   }
 
@@ -73,11 +85,11 @@ function draw() {
   var wavelength = 20;
   // 振幅
   var amplitude = height/4;
-  if (frameCount % 30==0) {
-    var index = floor(random(4));
-    var origin = createVector(random(- width + 10,width*2),random(-height,2*height));
-    waveSystem.waves[index] = new Wave(origin,random(-10,10),random(width/5,width/2),random(2,10));
-  }
+  // if (frameCount % 30==0) {
+  //   var index = floor(random(4));
+  //   var origin = createVector(random(- width + 10,width*2),random(-height,2*height));
+  //   waveSystem.waves[index] = new Wave(origin,random(-10,10),random(width/5,width/2),random(2,10));
+  // }
 
   var origin = createVector(-width,height/2);
   var randomWave = new Wave(origin,10,30,2);
@@ -87,7 +99,7 @@ function draw() {
     for (var y = 0; y< height;y+=pointSize) {
         var v = createVector(x,y);
         var z = waveSystem.resultZ(v);
-        z += randomWave.reachLocation(v);
+        // z += randomWave.reachLocation(v);
         // print(z);
         var alp = map(z,-50,50,-255,255);
 
